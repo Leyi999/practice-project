56cd9e7ec3419c60bd9cae286195e237b7596fb4
leyi_999 <1779579409@qq.com>
2021-11-24 23:46:03 +0800

    项目代码上传

---
+++ 乐意的扫雷游戏/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "game.h"
+#include"print.h"
+
+int main() {
+	srand((unsigned int)time(NULL));
+	int input;
+	do {
+		menu();
+		printf("请选择序号:>");
+		scanf("%d", &input);
+		switch (input) {
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		case 2:
+			INFOR();
+			break;
+		default:
+			printf("输入错误 重新输入\n");
+		}
+
+	} while (input);
+
+	return 0;
+}
